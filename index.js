3a3a6f25ac99ff99707ca1134b02e229a3a4ce29
Mike Sorokin <usamike1998@gmail.com>
2026-09-14 09:55:58 -0700

    Connected js to actually secure the vault

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ let code3 = 13 * 3;
 let message = "The vault has been secured. The combination is:";
 let codeA = code1 + "-" + code2 + "-" + code3;
 let codeB = `${code1}-${code2}-${code3}`;
+console.log(message);
+console.log(codeA);
+console.log(codeB);
 
 console.log(message + " " + codeA);
 console.log(`${message} ${codeB}`);
-console.log(codeA);
-console.log(codeB);
